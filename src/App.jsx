13fa7f453ca6ac4271e216959daeb05b5eaee761
JEDIWONG <126782636+JEDIWONG@users.noreply.github.com>
2024-08-19 22:46:04 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./component/Layout";
 import Home from "./component/Home";
-import "./style/App.css";
 import FoodPage from "./component/FoodPage";
 import Cart from "./component/Cart";
+import React, { createContext, useState } from "react";
+import Success from "./component/Success";
+
+export const UserCartContext = createContext();
 
 function App() {
+  const [userCart, setUserCart] = useState([]);
 
   return (
-    <>
+    <UserCartContext.Provider value={{ userCart, setUserCart }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/Kopi-Wong/" element={<Layout/>}>
-            <Route index element={<Home/>}/>
-            <Route path="cart" element={<Cart/>}></Route>
-            <Route path="food/:id" element={<FoodPage/>}/>
+          <Route path="/Kopi-Wong/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="food/:id" element={<FoodPage />} />
+            <Route path="success" element={<Success/>}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
-  )
+    </UserCartContext.Provider>
+  );
 }
 
 export default App;
